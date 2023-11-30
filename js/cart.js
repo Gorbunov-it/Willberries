bb8d994aad81184c49aac79c0903f1b2463c716c
Ivan Gorbunov <I.MGorbunov@yandex.ru>
2023-11-30 20:36:46 +0400

    revert

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,13 @@
-const funCart = (params) => {
+const funCart = function (params) {
   const btnCart = document.querySelector(".button-cart");
   const modal_cart = document.getElementById("modal-cart");
   const btnModalCart = modal_cart.querySelector(".modal-close");
 
-  btnCart.addEventListener("click", () => {
+  btnCart.addEventListener("click", function () {
     modal_cart.style.display = "flex";
   });
 
-  btnModalCart.addEventListener("click", () => {
+  btnModalCart.addEventListener("click", function () {
     modal_cart.style.display = "";
   });
 };
