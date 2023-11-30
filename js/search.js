826fb4cf1141bf8989eaff2e2b7ddd4a9dd3adf1
Ivan Gorbunov <I.MGorbunov@yandex.ru>
2023-11-30 20:47:02 +0400

    revert_

--- js/search.js
+++ js/search.js
@@ -2,46 +2,12 @@ const funSearch = function (params) {
   const input = document.querySelector(".search-block > input");
   const btnSeacrh = document.querySelector(".search-block > button");
 
-  const renderGoods = (goods) => {
-    const goodsContainer = document.querySelector(".long-goods-list");
-    goodsContainer.innerHTML = "";
+  input.addEventListener("input", function (event) {
+    console.log(event.target.value);
+  });
 
-    goods.forEach((good) => {
-      const goodBlock = document.createElement("div");
-      goodBlock.classList.add("col-lg-3");
-      goodBlock.classList.add("col-sm-6");
-      goodBlock.innerHTML = `
-      <div class="goods-card">
-        <span class="label ${good.label ? null : "d-none"}">${good.label}</span>
-          <img src="db/${good.img}" alt="${good.name}" class="goods-image" />
-          <h3 class="goods-title">${good.name}</h3>
-          <p class="goods-description">${good.description}</p>
-          <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
-            <span class="button-price">$${good.price}</span>
-          </button>
-      </div>`;
-
-      goodsContainer.append(goodBlock);
-    });
-  };
-
-  const getData = (value) => {
-    fetch("./db/db.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const array = data.filter((good) => good.name.toLowerCase().includes(value.toLowerCase()));
-        localStorage.setItem("goods", JSON.stringify(array));
-
-        if (window.location.pathname !== "/goods.html") {
-          window.location.href = "/goods.html";
-        } else {
-          renderGoods(array);
-        }
-      });
-  };
-
-  btnSeacrh.addEventListener("click", () => {
-    getData(input.value);
+  btnSeacrh.addEventListener("click", function () {
+    console.log(input.value);
   });
 };
 
